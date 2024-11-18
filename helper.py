6478a60b3225019ee3102b09cc12377a4dbc3d11
Giovanni Innamorato <giovanni11206@gmail.com>
2024-11-18 09:21:06 +0100

    Added succesfully hepler test

--- helper.py
+++ helper.py
@@ -1,18 +1,20 @@
 from dataclasses import dataclass
+from datetime import datetime, date
 
 items = []
-
 
 @dataclass
 class Item:
     text: str
+    due_date: date
     isCompleted: bool = False
 
 
-def add(text):
+def add(text, due_date):
+    # Konvertiere das Datum in ein datetime.date-Objekt
+    parsed_date = datetime.strptime(due_date, '%Y-%m-%d').date()
     text = text.replace('b', 'bbb').replace('B', 'Bbb')
-    items.append(Item(text))
-
+    items.append(Item(text, parsed_date))
 
 def get_all():
     return items
